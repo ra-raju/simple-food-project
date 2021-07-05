7fa3d7c01a820c5ce1e2d898a2a817f82bba30b1
robeyoul-awal-raju <robeyoulawal@gmail.com>
2021-07-05 15:34:47 +0600

    add without navbar

---
+++ main.js
@@ -0,0 +1,17 @@
+let icons = document.querySelectorAll('.section-1-icons i');
+
+let i = 1;
+
+setInterval(() => {
+  i++;
+  console.log(i);
+  let icon = document.querySelector('.section-1-icons i.change');
+  icon.classList.remove('change');
+
+  if (i > icons.length) {
+    icons[0].classList.add('change');
+    i = 1;
+  } else {
+    icon.nextElementSibling.classList.add('change');
+  }
+}, 3000);
